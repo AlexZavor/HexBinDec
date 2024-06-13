840ef2229627b67a84edd89f59db7afcf82dbbe7
Alexzavor <enderdefender9@gmail.com>
2024-06-13 12:51:30 -0600

    Fixed math, added operations

--- Math.gd
+++ Math.gd
@@ -32,10 +32,14 @@ func is_valid_bin(string):
 	return true
 
 func eval_bin(dec):
+	var neg = false
 	var string = ""
 	var i = 1
 	if(dec == 0):
 		return "0"
+	elif(dec < 0):
+		neg = true
+		dec = abs(dec)-1
 	while (dec > 0):
 		if(dec % (1<<i)):
 			string = "1"+string
@@ -45,7 +49,31 @@ func eval_bin(dec):
 		i+=1
 		if(((i-1) % 4 == 0) && (dec > 0)):
 			string = " "+string
+	if neg:
+		var string2 = "1"
+		for c in string:
+			if(c == "0"):
+				string2+="1"
+			elif(c == "1"):
+				string2+="0"
+			else:
+				string2+=" "
+		string = string2
 	return string
+
+func eval_twos_bin(bin):
+	var rtn = 0
+	var i = pow(2, bin.length()-1)
+	var neg = true
+	for c in bin:
+		if(c == "1"):
+			if(neg):
+				rtn -= i
+			else:
+				rtn += i
+		i /= 2
+		neg = false
+	return rtn
 
 func to_hex_char(input):
 	if(input>9):
@@ -67,11 +95,52 @@ func to_hex_char(input):
 	else:
 		return str(input)
 
+func invert_hex(input):
+	match input:
+		"0":
+			return "F"
+		"1":
+			return "E"
+		"2":
+			return "D"
+		"3":
+			return "C"
+		"4":
+			return "B"
+		"5":
+			return "A"
+		"6":
+			return "9"
+		"7":
+			return "8"
+		"8":
+			return "7"
+		"9":
+			return "6"
+		"A":
+			return "5"
+		"B":
+			return "4"
+		"C":
+			return "3"
+		"D":
+			return "2"
+		"E":
+			return "1"
+		"F":
+			return "0"
+		_:
+			return "X"
+
 func eval_hex(dec):
+	var neg = false
 	var string = ""
 	var i = 1
 	if(dec == 0):
 		return "0"
+	elif(dec < 0):
+		neg = true
+		dec = abs(dec)-1
 	while (dec > 0):
 		var reference = pow(16,i)
 		reference = dec % int(reference)
@@ -83,10 +152,20 @@ func eval_hex(dec):
 		i+=1
 		if(((i-1) % 4 == 0) && (dec > 0)):
 			string = " "+string
+	if neg:
+		if(string.length() == 0):
+			return "1"
+		if(string[0] >= "8"):
+			string = "0"+string
+		var string2 = ""
+		for c in string:
+			string2 += invert_hex(c)
+		string = string2
 	#string = "0x"+string
 	return string
 
 func calculate():
+	ansText.remove_theme_color_override("font_color")
 	match operationSel.selected:
 		0: # add
 			ans = op1 + op2
@@ -94,6 +173,25 @@ func calculate():
 		1: # sub
 			ans = op1 - op2
 			set_ans_text()
+		2: # mul
+			ans = op1 * op2
+			set_ans_text()
+		3: # div
+			if(op2 != 0):
+				ans = int(op1 / op2)
+				set_ans_text()
+			else:
+				ansText.add_theme_color_override("font_color", Color(1,0,0,1))
+				ans = 0
+				set_ans_text()
+		4: # mod
+			if(op2 != 0):
+				ans = op1 % op2
+				set_ans_text()
+			else:
+				ansText.add_theme_color_override("font_color", Color(1,0,0,1))
+				ans = 0
+				set_ans_text()
 
 # edit text
 
@@ -121,7 +219,7 @@ func _on_op_1_text_changed(new_text):
 				new_text = new_text.substr(2)
 			if is_valid_bin(new_text):
 				op1Text.remove_theme_color_override("font_color")
-				op1 = new_text.bin_to_int()
+				op1 = eval_twos_bin(new_text)
 			else:
 				op1Text.add_theme_color_override("font_color", Color(1,0,0,1))
 				op1 = 0
@@ -166,7 +264,7 @@ func _on_op_2_text_changed(new_text):
 				new_text = new_text.substr(2)
 			if is_valid_bin(new_text):
 				op2Text.remove_theme_color_override("font_color")
-				op2 = new_text.bin_to_int()
+				op2 = eval_twos_bin(new_text)
 			else:
 				op2Text.add_theme_color_override("font_color", Color(1,0,0,1))
 				op2 = 0
